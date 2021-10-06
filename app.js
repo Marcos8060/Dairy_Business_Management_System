9890072e9f814ce181c35e10b558966b1443caeb
Marcos Ochieng <marcosgav80@gmail.com>
2021-10-06 13:17:26 -0400

    added all the input fields with their Ids in the javascript file

---
+++ app.js
@@ -0,0 +1,5 @@
+const field1 = document.getElementById("num1");
+const field2 = document.getElementById("num2");
+const field3 = document.getElementById("num3");
+const field4 = document.getElementById("num4");
+const field5 = document.getElementById("num4");
