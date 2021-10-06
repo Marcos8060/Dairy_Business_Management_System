345db4ef6e1da8d4dcc47237464948ac65c3e219
Marcos Ochieng <marcosgav80@gmail.com>
2021-10-06 15:12:15 -0400

    Succesfully automated the addition functionality on the litres accrued from the four sheds

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const field1 = document.getElementById("num1");
 const field2 = document.getElementById("num2");
 const field3 = document.getElementById("num3");
 const field4 = document.getElementById("num4");
+const total = document.getElementById("total");
 
 // Event listener
 form.addEventListener('submit',getTotals);
