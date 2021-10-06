0523555d667b9f6ed390f073f608c5f051eeace6
Marcos Ochieng <marcosgav80@gmail.com>
2021-10-06 13:42:36 -0400

    Added the submit functionality in the javascript file

--- app.js
+++ app.js
@@ -6,4 +6,17 @@ const field4 = document.getElementById("num4");
 // Event listener
 form.addEventListener('submit',getTotals);
 
+// Total amount in litres
+function getTotals(e){
+   
+    const result = parseFloat(field1.value)+parseFloat(field2.value)
+                   +parseFloat(field3.value)+parseFloat(field4.value)
+
+    if(!isNaN(result)){
+       total.innerHTML = result+ " Litres";
+    }
+
+    e.preventDefault();
+}
+
 
