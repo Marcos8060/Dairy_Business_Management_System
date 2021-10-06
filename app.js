fa88d8047e81466ffd012987a9a0317ee30470bc
Marcos Ochieng <marcosgav80@gmail.com>
2021-10-06 15:43:53 -0400

    Succesfully achieved the yearly income functionality in the javascript file

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const field2 = document.getElementById("num2");
 const field3 = document.getElementById("num3");
 const field4 = document.getElementById("num4");
 const total = document.getElementById("total");
-const weekly = document.getElementById("weekly");
+const weeklyIncome = document.getElementById("weekly");
+const yearlyIncome = document.getElementById("yearly")
 
 // Event listener
 form.addEventListener('submit',getTotals);
@@ -30,14 +31,14 @@ function incomeOverTime(){
                    +parseFloat(field3.value)+parseFloat(field4.value) * 45 * 7;
 
     if(!isNaN(answer)){
-       weekly.innerHTML = "Your weekly income will be <br> ksh "+answer;
+       weeklyIncome.innerHTML = "Your weekly income will be <br> ksh "+answer;
     }
     // Yearly income
     const response = parseFloat(field1.value)+parseFloat(field2.value)
-                   +parseFloat(field3.value)+parseFloat(field4.value) * 45 * 7;
+                   +parseFloat(field3.value)+parseFloat(field4.value) * 45 * 7 * 52;
 
     if(!isNaN(response)){
-       weekly.innerHTML = "Your yearly income will be <br> ksh "+response;
+       yearlyIncome.innerHTML = "Your yearly income will be <br> ksh "+response;
     }
 }
 
