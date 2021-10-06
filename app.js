b7147a0b7db4125b14d04e4302531406a00c4b73
Marcos Ochieng <marcosgav80@gmail.com>
2021-10-06 13:25:12 -0400

    Added an event listener

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ const field1 = document.getElementById("num1");
 const field2 = document.getElementById("num2");
 const field3 = document.getElementById("num3");
 const field4 = document.getElementById("num4");
+
+// Event listener
+form.addEventListener('submit',getTotals);
+
+
